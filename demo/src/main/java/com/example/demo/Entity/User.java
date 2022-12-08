e8e4cc5ab7f32b5d9bd65a214dbefcf359f5aca3
nemo2134 <31640453+nemo2134@users.noreply.github.com>
2022-12-08 15:39:56 -0500

    user update

--- demo/src/main/java/com/example/demo/Entity/User.java
+++ demo/src/main/java/com/example/demo/Entity/User.java
@@ -1,13 +1,14 @@
 package com.example.demo.Entity;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="user_name")
+    @Column(name="user_name") //user id and make it UNIQUE type
     private String userName;
 
     @Column(name = "user_password")
@@ -15,7 +16,11 @@ public class User {
 
     @Column(name = "user_type")
     private String userType;
-    
+
+    @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.EAGER,orphanRemoval = true, mappedBy = "user")
+    private List<Product> product;
+
+    //user delete
 
     public User( String userName, String userPassword) {
         this.userName = userName;
