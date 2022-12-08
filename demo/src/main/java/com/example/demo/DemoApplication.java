ae6370ba4ee74b2e9f3b921953e9d6164409464f
nemo2134 <31640453+nemo2134@users.noreply.github.com>
2022-12-07 19:40:58 -0500

    Testing 2 Simon

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -15,7 +15,7 @@ SpringApplication.run(DemoApplication.class, args);
 }
 
 @GetMapping("/hello")
-public String hello(@RequestParam(value = "name", defaultValue = "World Itlize. Testing git") String name) {
+public String hello(@RequestParam(value = "name", defaultValue = "World Simon github testing!") String name) {
 return String.format("Hello %s!", name);
 }
 }
