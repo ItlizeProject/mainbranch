b69f12b331cffe015ac59da8bbc2a50ea71f867a
nemo2134 <31640453+nemo2134@users.noreply.github.com>
2022-12-12 01:35:55 -0500

    Project + User Repository and Service

---
+++ demo/src/main/java/com/example/demo/Service/ProjectServiceImpl.java
@@ -0,0 +1,54 @@
+package com.example.demo.Service;
+
+import com.example.demo.Entity.Project;
+import com.example.demo.Entity.User;
+import com.example.demo.Repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProjectServiceImpl implements ProjectService{
+    @Autowired
+    private ProjectRepository projectRepository;
+    @Override
+    public Project createProject(Project project) {
+        return projectRepository.save(project);
+    }
+
+    @Override
+    public Project save(Project project) {
+        return projectRepository.save(project);
+    }
+
+    @Override
+    public List<Project> findAll() {
+        return projectRepository.findAll();
+    }
+
+    @Override
+    public List<Project> findByUser(User user) {
+        return projectRepository.findByUser(user).orElse(null);
+    }
+
+    @Override
+    public Project findProjectByProjectId(long id) {
+        return projectRepository.findProjectById(id).orElse(null);
+    }
+
+    @Override
+    public String deleteProject(long id) {
+        projectRepository.deleteById(id);
+        return "Project "+id+" has been removed";
+    }
+
+    @Override
+    public Project findProjectByProjectProductId(long id) {
+        return projectRepository.findProjectByProjectProductId(id).orElse(null);
+    }
+
+//    @Override
+//    public void addProductToProject(Product product) {
+//    }
+}
