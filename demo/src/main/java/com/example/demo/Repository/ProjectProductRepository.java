216f04542dd5f923b71931f76955deaa16e2353f
nemo2134 <31640453+nemo2134@users.noreply.github.com>
2022-12-12 15:20:43 -0500

    projectproduct project user

--- demo/src/main/java/com/example/demo/Repository/ProjectProductRepository.java
+++ demo/src/main/java/com/example/demo/Repository/ProjectProductRepository.java
@@ -1,4 +1,13 @@
 package com.example.demo.Repository;
 
-public interface ProjectProductRepository {
+import com.example.demo.Entity.ProjectProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProjectProductRepository extends JpaRepository<ProjectProduct, Long>{
+    Optional<ProjectProduct> findProjectProductById(long id);
+
+    Optional<ProjectProduct> findProjectProductByProductId(long id);
+    Optional<ProjectProduct> findProjectProductByProjectId(long id);
 }
---
+++ demo/src/main/java/com/example/demo/Service/ProjectProductServiceImpl.java
@@ -0,0 +1,36 @@
+package com.example.demo.Service;
+
+import com.example.demo.Entity.ProjectProduct;
+import com.example.demo.Repository.ProjectProductRepository;
+
+import java.util.List;
+
+public class ProjectProductServiceImpl implements ProjectProductService{
+
+    ProjectProductRepository projectProductRepository;
+    @Override
+    public ProjectProduct createProjectProduct(ProjectProduct projectProduct) {
+        return projectProductRepository.save(projectProduct);
+    }
+
+    @Override
+    public ProjectProduct save(ProjectProduct projectProduct) {
+        return projectProductRepository.save(projectProduct);
+    }
+
+    @Override
+    public String deleteProjectProduct(long id) {
+        projectProductRepository.deleteById(id);
+        return "Project Product "+ id +" has been removed";
+    }
+
+    @Override
+    public ProjectProduct findProjectProductById(long id) {
+        return projectProductRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<ProjectProduct> findAll() {
+        return projectProductRepository.findAll();
+    }
+}
