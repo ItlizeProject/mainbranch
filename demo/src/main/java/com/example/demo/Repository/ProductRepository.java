cd8697abeca5ca1c2b4fb5152695f21cd4805b9b
qyi <qingyi.y11@gmail.com>
2022-12-12 12:41:15 -0500

    Repository: Product & TechDetail

--- demo/src/main/java/com/example/demo/Repository/ProductRepository.java
+++ demo/src/main/java/com/example/demo/Repository/ProductRepository.java
@@ -1,4 +1,11 @@
 package com.example.demo.Repository;
 
-public interface ProductRepository {
+import com.example.demo.Entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long > {
+    Optional<Product> findProductByProductBrand(String productBrand);
+    Optional<Product> findProductByCertification(String certification);
 }
--- demo/src/main/java/com/example/demo/Repository/TechnicalDetailRepository.java
+++ demo/src/main/java/com/example/demo/Repository/TechnicalDetailRepository.java
@@ -1,4 +1,12 @@
 package com.example.demo.Repository;
 
-public interface TechnicalDetailRepository {
+import com.example.demo.Entity.Product;
+import com.example.demo.Entity.TechnicalDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.Optional;
+
+public interface TechnicalDetailRepository extends JpaRepository<TechnicalDetail, Long> {
+
 }
