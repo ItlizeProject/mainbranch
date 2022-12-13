e9ea9af06923c3f0d5a408f3c8299a43d091b870
VcctoriaShao <73573406+ZFA20SCM38S@users.noreply.github.com>
2022-12-12 21:12:53 -0600

    service update

--- demo/src/main/java/com/example/demo/Repository/DescriptionRepository.java
+++ demo/src/main/java/com/example/demo/Repository/DescriptionRepository.java
@@ -7,7 +7,10 @@ import java.util.*;
 
 public interface DescriptionRepository extends JpaRepository<Description, Integer>{
 
-    // 声明repository的methods（写了名字之后jpa会自动帮我实现）
+    // 声明repository的methods（写了名字之后jpa会自动帮我实现,起名很关键，比如写findByProductId，Jpa就会自动帮我实现通过ProductId在Description这张table里
+    // 查找符合要求的tuple）
+    //下面这些内容本质上定义了我想给用户提供的用来搜索整张Description table的index（比如manufacturer是Description表的一个attribute。本身对于表来说查找所用的
+    // index应该是primary key。但是我想给user提供一个通过输入Manufacturer,找到表里对应符合要求的tuple的功能，所以我在这里写了findByManufacturer这个方法）
 
     Optional<Description> findBy(Integer id);
     Optional<Description> findByProductId(Integer id);
