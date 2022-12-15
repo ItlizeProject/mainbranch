a6834ecf49f75bded45d8102efab63e5362e8515
qyi <qingyi.y11@gmail.com>
2022-12-15 17:30:33 -0500

    TechDetail Repository

--- demo/src/main/java/com/example/demo/Repository/TechnicalDetailRepository.java
+++ demo/src/main/java/com/example/demo/Repository/TechnicalDetailRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface TechnicalDetailRepository extends JpaRepository {
+public interface TechnicalDetailRepository extends JpaRepository<TechnicalDetail, Integer> {
     Optional<List<TechnicalDetail>> findProductByAirflowBetween(Integer para1, Integer para2);
     Optional<List<TechnicalDetail>> findProductByPowerBetween(Integer para1, Integer para2);
     Optional<List<TechnicalDetail>> findProductByOperatingVoltageBetween(Integer para1, Integer para2);
